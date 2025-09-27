e0ea6bd194efad9fae0d65dc1a80ad4afc93d4f8
Nicole C. <nmcanson@gmail.com>
2025-09-27 14:41:09 -0700

    Second commit

---
+++ Lab-14.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// Color class
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+
+public:
+    // Setters
+    void setRed(int r)      { red = r; }
+    void setGreen(int g)    { green = g; }
+    void setBlue(int b)     { blue = b; }
+
+    // Getters
+    int getRed()            { return red; }
+    int getGreen()          { return green; }
+    int getBlue()           { return blue; }
+
+    // Print method
+    void print();
+};
+
+int main() {
+    Color color1;
+    Color color2;
+    Color color3;
+
+    // Set values
+
+    return 0;
+}
